72928bb204ead6bcd9c48a4d58fa08e956de1e07
Abel Zeru H/selassie <abelzeruhaileselassie@gmail.com>
2025-12-14 21:49:23 +0300

    dfdjhdf

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const mime = require(mime-types)
  const server = http.createServer((req,res) => {
 const reqUrl = req.url
 const sendfile = reqUrl = reqUrl == '/' ? 'index.html' : reqUrl;
-const sendfiepath = path.join(__dirname,sendfile);
+const sendfiepath = path.join(__dirname,'portf',sendfile);
  const contentType = mime.lookup(sendfile) || 'text/htm'
 
 fs.readFile(sendfiepath,(err,data) => {
